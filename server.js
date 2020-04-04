2a2d70aa85a33ffbdaf8bd3814457740521f9690
Onur DEGERLI <onurdegerli@gmail.com>
2020-04-04 21:28:21 +0300

    Changing port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-serveHTTP(addonInterface, { port: 7000 });
+serveHTTP(addonInterface, { port: 3000 });
 
 publishToCentral('https://onurdegerli-stremio-oscar-movies.glitch.me/manifest.json')
